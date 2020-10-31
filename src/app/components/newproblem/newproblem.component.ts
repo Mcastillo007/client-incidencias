fdfcbf88a24db46b5a7c3b068856cd7cffebf474
Matias Amestica <matias.amestica@alu.ucm.cl>
2020-10-31 18:06:17 -0300

    :D

--- src/app/components/newproblem/newproblem.component.ts
+++ src/app/components/newproblem/newproblem.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { NavParams, ModalController } from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
+import { CategoryService } from '../../services/category.service';
+import { SubcategoryService } from '../../services/subcategory.service';
+import { SubjectService } from '../../services/subject.service';
 
 @Component({
   selector: 'app-newproblem',
@@ -7,8 +13,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewproblemComponent implements OnInit {
 
-  constructor() { }
+  problem: any;
+  problemForm: FormGroup;
+  isSubmitted = false;
+  categories: any = [];
+  subcategories: any = [];
+  subjects: any = [];
+
+  constructor(
+    private apiService: ApiService,
+    private modalController: ModalController,
+    private formBuilder: FormBuilder,
+    private NavParams: NavParams,
+    private categoryService: CategoryService,
+    private subcategoryService: SubcategoryService,
+    private subjectService: SubjectService,
+
+  ) {
+    this.categories = NavParams.get('categories');
+    this.subcategories = NavParams.get('subcategories');
+    this.subjects = NavParams.get('subjects');
+    this.problemForm = new FormGroup({
+      'description': new FormControl(null, [Validators.required, Validators.minLength(25)]),
+    });
+  }
 
   ngOnInit() {}
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.problemForm.valid) {
+      console.log('Complete correctamente todos los campos');
+      return false;
+    } else {
+      let problem = this.problemForm.value;
+      this.apiService.post('problem', problem).then(res => {
+        this.modalController.dismiss();
+      }).catch(res => {
+
+      });
+      console.log(this.problem);
+    }
+  }
 
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { NewproblemComponent } from 'src/app/components/newproblem/newproblem.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  problem: any = {};
+  categories: any = [];
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private modalController: ModalController,
+    private categoryService: CategoryService,
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.getCategories();
   }
-
+  async newproblem(){
+    let createModal = await this.modalController.create({
+      component: NewproblemComponent,
+      componentProps: {
+        categories: this.categories
+      }
+    });
+    createModal.onDidDismiss().then(res => {
+    }); 
+    return await createModal.present();
+  }
+  async getCategories() {
+    try {
+      let categoriesResponse: any = await this.categoryService.getAll();
+      if (null != categoriesResponse) {
+        this.categories = categoriesResponse.categories;
+        console.log(this.categories);
+      }
+    } catch (ex) {
+      console.log(ex.error.message)
+    }
+  }
 }
